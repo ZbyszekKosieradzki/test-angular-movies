588c3d8558eaa5829f676abad30b991997bbf8b9
Zbyszek Kosieradzki <bossard7@gmail.com>
2019-01-09 08:32:21 +0100

    first commit

--- src/app/services/movies-provider.service.ts
+++ src/app/services/movies-provider.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import {environment} from '../../environments/environment';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +17,7 @@ export class MoviesProviderService {
   }
 
   fetchMovies() {
-    this.http.get('/assets/movies.json').toPromise()
+    this.http.get(environment.moviesUrl).toPromise()
       .then((response) => {
         console.log(response);
       });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  moviesUrl:'test-angular-movies/assets/movies.json'
 };
