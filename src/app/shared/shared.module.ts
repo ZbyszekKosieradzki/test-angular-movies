1ddd2ebd5fa482df20029b13938d6b66fb44b5b0
Zbyszek Kosieradzki <bossard7@gmail.com>
2019-01-10 12:56:40 +0100

    commit 14

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SearchFormComponent } from './components/search-form/search-form.component';
+
 
 import { FormsModule } from '@angular/forms';
 import { ImageComponent } from './components/image/image.component';
+import { SearchFormComponent } from '../components/search-form/search-form.component';
 
 
 @NgModule({
